ba87713a9cc24cbe3f3e311ea7636995e15af03e
Jeremy <markjeremyj@gmail.com>
2025-06-04 14:38:27 +0300

    adds conditionals

--- index.js
+++ index.js
@@ -1,11 +1,47 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+   let scuberGreetingForFeet
+   if (someValue<=400){
+    scuberGreetingForFeet ='This one is on me!'
+    return scuberGreetingForFeet
+
+   }
+   else if (someValue > 400 && someValue < 2000){
+    scuberGreetingForFeet ='That will be twenty bucks.'
+    return scuberGreetingForFeet
+   }
+   else if (someValue>2000 && someValue<2500){
+    scuberGreetingForFeet ='I will gladly take your thirty bucks.'
+    return scuberGreetingForFeet
+  }
+  else (someValue>2500);{
+    scuberGreetingForFeet ='No can do.'
+    return scuberGreetingForFeet
+  }
+
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(someCity){
+
+    someCity === 'NYC' ? (ternaryCheckCity='Ok, sounds good.') : (ternaryCheckCity='No go.')
+    return ternaryCheckCity
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip){
+    case 'generous':
+      switchOnCharmFromTip ='Thank you so much.'
+      return switchOnCharmFromTip
+
+    case 'not as generous':
+      switchOnCharmFromTip = 'Thank you.'
+      return switchOnCharmFromTip
+
+    default:
+      switchOnCharmFromTip = 'Bye.'
+      return switchOnCharmFromTip
+
+
+
+
+  }
 }
